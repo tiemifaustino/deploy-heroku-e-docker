37d501438456572317c051c7cf61c7364e44f8c6
Tiemi Faustino <hayashipharma@gmail.com>
2022-07-22 15:43:31 -0300

    alterando index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send(`<h1> Executando na porta ${PORT} </h1>`));
+app.get('/', (req, res) => res.send(`<h1> Executando com DOCKER na porta ${PORT} </h1>`));
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
